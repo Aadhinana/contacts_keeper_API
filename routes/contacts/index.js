692a5af5ae61d0da629702fde21cd9e9a4a3a9d1
Aadhi <ajibaa@gmail.com>
2020-11-05 21:06:25 +0530

    create udpate delete contact API

--- routes/contacts/index.js
+++ routes/contacts/index.js
@@ -1,10 +1,11 @@
 const express = require("express");
 
 const authMiddleware = require("../../middleware/authentication");
-
+const { body, validationResult } = require("express-validator");
 const router = express.Router();
 
 const Contact = require("../../models/contact");
+const { update } = require("../../models/contact");
 
 /* 
 / GET 
@@ -27,17 +28,68 @@ router.get("/", authMiddleware, async (req, res) => {
 Adds a contact for the logged in user
 protected
 */
-router.post("/", authMiddleware, (req, res) => {
-  console.log("Adds a contact for the user");
-});
+router.post(
+  "/",
+  authMiddleware,
+  [body("name", "Name cannot be empty field").not().isEmpty()],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors });
+    }
+
+    const newContact = {
+      name: req.body.name,
+      email: req.body.email || " ",
+      phone: req.body.phone || " ",
+      user: req.user,
+    };
+    try {
+      const contact = new Contact(newContact);
+      const result = await contact.save();
+
+      return res.status(201).json({ contact: result });
+    } catch (error) {
+      return res.status(500).json({ message: "Server Error" });
+    }
+  }
+);
 
 /* 
 /:id PUT
 update a contact with the user ID
 protected
 */
-router.put("/:id", authMiddleware, (req, res) => {
-  console.log("Updates a contact");
+router.put("/:id", authMiddleware, async (req, res) => {
+  // find the contact wiht the id
+  try {
+    const contactData = await Contact.findOne({ _id: req.params.id });
+
+    if (!contactData) {
+      return res.status(404).json({ message: `No such contact found!` });
+    }
+
+    //check if contact belongs to user
+    if (contactData.user != req.user) {
+      return res
+        .status(401)
+        .json({ message: "You are not Authorized to do this." });
+    }
+
+    const updatedContact = {
+      name: req.body.name || contactData.name,
+      email: req.body.email || contactData.email,
+      phone: req.body.phone || contactData.phone,
+    };
+
+    const result = await Contact.updateOne(
+      { _id: req.params.id },
+      updatedContact
+    );
+    return res.status(200).json({ message: "Contact updated" });
+  } catch (error) {
+    return res.status(500).json({ message: "Server Error" });
+  }
 });
 
 /* 
@@ -45,8 +97,27 @@ router.put("/:id", authMiddleware, (req, res) => {
 delete a contact with the user ID
 protected
 */
-router.delete("/:id", authMiddleware, (req, res) => {
-  console.log("deletes a contact");
+router.delete("/:id", authMiddleware, async (req, res) => {
+  // find the contact wiht the id
+  try {
+    const contactData = await Contact.findOne({ _id: req.params.id });
+
+    if (!contactData) {
+      return res.status(404).json({ message: `No such contact found!` });
+    }
+
+    //check if contact belongs to user
+    if (contactData.user != req.user) {
+      return res
+        .status(401)
+        .json({ message: "You are not Authorized to do this." });
+    }
+
+    const result = await Contact.deleteOne({ _id: req.params.id });
+    return res.status(200).json({ message: "Contact deleted" });
+  } catch (error) {
+    return res.status(500).json({ message: "Server Error" });
+  }
 });
 
 module.exports = router;
